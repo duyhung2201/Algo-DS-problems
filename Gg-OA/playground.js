b9f7f59cc5aee89d0572a811c76f07bc0a859160
DuyHungLe <duyhung2201@gmail.com>
2024-08-29 13:31:41 -0400

    feat: Implement solution for p300_longest_increasing_subsequence.js

--- Gg-OA/playground.js
+++ Gg-OA/playground.js
@@ -1,60 +1,21 @@
-function minDaysBloom(roses, k, n) {
-	const isEnoughBouquets = (day) => {
-		let adjacent = 0,
-			bouquets = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+	let n = nums.length;
+	let dp = Array(n + 1).fill(1);
 
-		for (const rose of roses) {
-			if (rose <= day) {
-				adjacent++;
-			} else {
-				adjacent = 0;
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = 0; j < i; j++) {
+			if (nums[j] < nums[i]) {
+				dp[i] = Math.max(dp[i], dp[j] + 1);
 			}
-			if (adjacent === k) {
-				bouquets++;
-				adjacent = 0;
-			}
-			if (bouquets == n) return true;
-		}
-		return false;
-	};
-
-	let left = Math.min(...roses),
-		right = Math.max(...roses);
-
-	while (left < right) {
-		const mid = Math.floor((left + right) / 2);
-		if (isEnoughBouquets(mid)) {
-			right = mid;
-		} else {
-			left = mid + 1;
 		}
 	}
-	return right;
-}
+	return Math.max(...dp);
+};
 
-// Input: roses = [1, 2, 4, 9, 3, 4, 1], k = 2, n = 2
-// Output: 4
-const roses = [1, 2, 4, 9, 3, 4, 1];
-const k = 2;
-const n = 2;
-
-console.log(minDaysBloom(roses, k, n));
-console.log(minDaysBloom([1], 1, 1));
-
-console.log(minDaysBloom([5, 5, 5, 5, 5], 2, 2));
-
-console.log(minDaysBloom([1, 2, 3, 4, 5], 2, 3));
-
-console.log(minDaysBloom([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2, 3));
-
-console.log(minDaysBloom([1, 1, 1, 1, 1], 2, 2));
-
-console.log(minDaysBloom([1, 10, 20, 30, 40], 4, 1));
-
-console.log(minDaysBloom([1, 100, 100, 100, 100], 2, 1));
-
-console.log(minDaysBloom([1, 2, 2, 2, 2], 4, 1));
-
-console.log(minDaysBloom([5, 4, 3, 2, 1], 2, 2));
-
-console.log(minDaysBloom([1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9], 2, 3));
+console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
+console.log(lengthOfLIS([0, 1, 0, 3, 2, 3]));
+console.log(lengthOfLIS([7, 7, 7, 7, 7, 7, 7]));
